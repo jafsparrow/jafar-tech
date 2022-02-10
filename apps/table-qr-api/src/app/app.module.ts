2fc3f30b6ef7ec1fd3870ff8e5b039aa8e0fb4ed
jafarcees@gmail.com <jafarcees@gmail.com>
2022-02-11 03:13:55 +0530

    feat: update the app module for products module

--- apps/table-qr-api/src/app/app.module.ts
+++ apps/table-qr-api/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BackendAuthModule } from '@jafar-tech/backend/auth';
 import { BackendCoreModule } from '@jafar-tech/backend/core';
+import { BackendProductsModule } from '@jafar-tech/backend/products';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [BackendCoreModule, BackendAuthModule],
+  imports: [BackendCoreModule, BackendAuthModule, BackendProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
