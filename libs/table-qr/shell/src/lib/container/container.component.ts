4e3d2e29e9a38629621c6d19465cac57a61486af
jafarcees@gmail.com <jafarcees@gmail.com>
2022-02-15 08:33:19 +0530

    refactor: added the ui parts

--- libs/table-qr/shell/src/lib/container/container.component.ts
+++ libs/table-qr/shell/src/lib/container/container.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '@jafar-tech/shared/data-access';
-import { addToCart, selectCart } from '@jafar-tech/table-qr-cart-data-access';
+import {
+  addToCart,
+  selectCart,
+  removeFromCart,
+  selectInCartProductCount,
+  selectNumberOfItemsInCart,
+} from '@jafar-tech/table-qr-cart-data-access';
 import {
   loadProducts,
   loadProductsCategoryVice,
   selectAllProducts,
   selectProductByCategories,
-  selectProductconsideringcart,
 } from '@jafar-tech/table-qr-products-data-access';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'jafar-tech-container',
@@ -18,7 +24,7 @@ import { Store } from '@ngrx/store';
 export class ContainerComponent implements OnInit {
   allProducts$ = this.store.select(selectAllProducts);
   prodcutByCategory$ = this.store.select(selectProductByCategories);
-  cart$ = this.store.select(selectCart);
+  cartCount$ = this.store.select(selectNumberOfItemsInCart);
 
   count = 5;
 
@@ -27,8 +33,12 @@ export class ContainerComponent implements OnInit {
   ngOnInit(): void {
     this.store.dispatch(loadProducts());
     this.store.dispatch(loadProductsCategoryVice());
+    this.cartCount$.subscribe((count) => console.log(count));
   }
 
+  getInCartProductCount(product: Product): Observable<any> {
+    return this.store.select(selectInCartProductCount, { id: product._id });
+  }
   openProductViewDialog() {}
 
   addToCart(product: Product) {
@@ -37,5 +47,14 @@ export class ContainerComponent implements OnInit {
       count: 1,
     };
     this.store.dispatch(addToCart({ item: cartItem }));
+  }
+
+  removeFromCart(product: Product) {
+    const cartItem = {
+      product: product,
+      count: 1,
+    };
+
+    this.store.dispatch(removeFromCart({ itemId: product._id }));
   }
 }
