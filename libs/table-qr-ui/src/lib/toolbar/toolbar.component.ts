0f5f70e03fbc788e2029a804ca3b5dbee4ecdd25
jafarcees@gmail.com <jafarcees@gmail.com>
2022-02-15 08:31:32 +0530

    refactor: toolbar css and added input count property

--- libs/table-qr-ui/src/lib/toolbar/toolbar.component.ts
+++ libs/table-qr-ui/src/lib/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'qr-ui-toolbar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.css'],
 })
 export class ToolbarComponent implements OnInit {
+  @Input('cartItemCount') cartCount?: number | null;
+
   constructor() {}
 
   ngOnInit(): void {}
