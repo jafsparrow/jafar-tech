74c45a0a02ff8dd154d6fe3ef733d27a1441104b
jafarcees@gmail.com <jafarcees@gmail.com>
2022-02-16 10:20:14 +0530

    refactor: updating the cart model

--- libs/shared/data-access/src/lib/models/cart.ts
+++ libs/shared/data-access/src/lib/models/cart.ts
@@ -1,4 +1,5 @@
 import { Product } from '.';
+import { User } from './user';
 
 export interface CartItem {
   product: Product;
@@ -7,6 +8,13 @@ export interface CartItem {
 
 export interface Cart {
   createdAt?: Date;
+  user?: User;
   cartItems: { [key: string]: CartItem };
+  pendingOders?: OrderSummary[];
   total: number;
+}
+
+export interface OrderSummary {
+  _id: string;
+  date?: Date;
 }
