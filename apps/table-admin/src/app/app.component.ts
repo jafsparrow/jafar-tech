fa902f858b81720192001281222f035271e4d4fc
jafarcees@gmail.com <jafarcees@gmail.com>
2022-02-21 10:08:10 +0530

    feat: added table admin app

---
+++ apps/table-admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jafar-tech-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'table-admin';
+}
