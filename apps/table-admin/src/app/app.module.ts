0e174dc474832d39a1d3f71c48e043ab7d8da37a
jafarcees@gmail.com <jafarcees@gmail.com>
2022-02-24 00:37:47 +0530

    refactor: imported ngrx and httpclient

--- apps/table-admin/src/app/app.module.ts
+++ apps/table-admin/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,6 +25,9 @@ import { AppComponent } from './app.component';
           ),
       },
     ]),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
