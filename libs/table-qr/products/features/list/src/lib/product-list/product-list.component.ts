a002e1e7b740b2c4e7d24471ad0cde55d0396e30
jafarcees@gmail.com <jafarcees@gmail.com>
2022-02-28 03:42:48 +0530

    fix: dialog size fix

--- libs/table-qr/products/features/list/src/lib/product-list/product-list.component.ts
+++ libs/table-qr/products/features/list/src/lib/product-list/product-list.component.ts
@@ -37,6 +37,10 @@ export class ProductListComponent implements OnInit {
   }
   openProductViewDialog(product: Product) {
     this.dialog.open(ProductDetailComponent, {
+      maxWidth: '95vw',
+      maxHeight: '95vh',
+      height: '100%',
+      width: '100%',
       data: {
         product,
       },
