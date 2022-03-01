69c8796218aa5472ffe2457a59cf411706ef2c0b
jafarcees@gmail.com <jafarcees@gmail.com>
2022-03-01 13:21:35 +0530

    refactor: UI change for bottom cart summary

--- libs/table-qr-ui/src/lib/bottom-cart-summary/bottom-cart-summary.component.ts
+++ libs/table-qr-ui/src/lib/bottom-cart-summary/bottom-cart-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'qr-ui-bottom-cart-summary',
@@ -8,7 +8,6 @@ import { Component, Input, OnInit, Output } from '@angular/core';
 export class BottomCartSummaryComponent implements OnInit {
   @Input('cartProductsCount') count?: number | null;
   @Input('cartTotal') total?: number | null;
-
   constructor() {}
 
   ngOnInit(): void {}
