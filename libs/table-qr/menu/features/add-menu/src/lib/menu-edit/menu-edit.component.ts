f2aac5b7ddeb7e42fbe7c767edd2e684a3696dcb
jafarcees@gmail.com <jafarcees@gmail.com>
2022-03-15 15:40:01 +0530

    refactor: chore remove debug messages

--- libs/table-qr/menu/features/add-menu/src/lib/menu-edit/menu-edit.component.ts
+++ libs/table-qr/menu/features/add-menu/src/lib/menu-edit/menu-edit.component.ts
@@ -15,31 +15,11 @@ import { Observable, ObservableLike, take, tap } from 'rxjs';
   styleUrls: ['./menu-edit.component.css'],
 })
 export class MenuEditComponent implements OnInit {
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker',
-  ];
-
   categories$ = this.store.select(selectCategories);
-  products: Product[] = [];
 
   constructor(private store: Store, private dragDropService: DragDrop) {}
 
   ngOnInit(): void {
     console.log('oninit fired');
-  }
-
-  getProductsOfCategory(category: string): Observable<Product[]> {
-    console.log('getting product from template fired');
-    return this.store
-      .select(selectProductsFromCategory(category))
-      .pipe(tap((data) => console.log('in tap', data)));
   }
 }
