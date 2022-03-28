77d278f3d227ba1fdf3f3cee4c06b273d30b20bc
jafarcees@gmail.com <jafarcees@gmail.com>
2022-03-28 18:14:36 +0530

    feat: category add component geenrated

--- libs/table-qr/category/features/add/src/lib/table-qr-category-features-add.module.ts
+++ libs/table-qr/category/features/add/src/lib/table-qr-category-features-add.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoryAddComponent } from './category-add/category-add.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    CategoryAddComponent
+  ],
 })
 export class TableQrCategoryFeaturesAddModule {}
