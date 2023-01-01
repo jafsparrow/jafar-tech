9c53397a3b7a9fec97d0eb03dee69f2f06fd47d7
jafsparrow <jafarcees@gmail.com>
2023-01-01 15:14:23 +0530

    add: created table-add-component

---
+++ libs/table-qr/table-management/features/table-add/src/lib/table-add/table-add.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jafar-tech-table-add',
+  templateUrl: './table-add.component.html',
+  styleUrls: ['./table-add.component.scss'],
+})
+export class TableAddComponent {}
--- libs/table-qr/table-management/features/table-add/src/lib/table-qr-table-management-features-table-add.module.ts
+++ libs/table-qr/table-management/features/table-add/src/lib/table-qr-table-management-features-table-add.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableAddComponent } from './table-add/table-add.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild([{
+    path: '', component: TableAddComponent
+  }])],
+  declarations: [TableAddComponent],
 })
 export class TableQrTableManagementFeaturesTableAddModule {}
